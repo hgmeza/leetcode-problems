2d58a6bdcf6c0e06258a2ce63eb0cb83b1662b3f
Hugo Meza <hgmeza114@gmail.com>
2019-07-23 18:59:09 -0400

    solve distributeCandies

---
+++ week2/distributeCandies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+      sort(candies.begin() , candies.end());
+      int count = 1;
+      
+      for (int i=1; i < candies.size() && count < candies.size() / 2; i++)
+        count += (candies[i] > candies[i-1]) ;
+      
+      return count ;
+    }
+};
